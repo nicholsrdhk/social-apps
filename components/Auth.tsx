961a12b7785fc4959585af20fc4dce04ba3b8e2a
FathanOswaldy <mvigilante112@gmail.com>
2025-09-22 20:53:10 +0700

    Error

--- components/Auth.tsx
+++ components/Auth.tsx
@@ -1,12 +1,8 @@
 import React, { useState } from 'react'
-import { Alert, StyleSheet, View, AppState } from 'react-native'
+import {  StyleSheet, View, AppState, Button, TextInput } from 'react-native'
 import { supabase } from '../lib/supabase'
-import { Button, Input } from '@rneui/themed'
+import { AlertDialog } from "./global/Alert"
 
-// Tells Supabase Auth to continuously refresh the session automatically if
-// the app is in the foreground. When this is added, you will continue to receive
-// `onAuthStateChange` events with the `TOKEN_REFRESHED` or `SIGNED_OUT` event
-// if the user's session is terminated. This should only be registered once.
 AppState.addEventListener('change', (state) => {
   if (state === 'active') {
     supabase.auth.startAutoRefresh()
@@ -18,40 +14,68 @@ AppState.addEventListener('change', (state) => {
 export default function Auth() {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
-  const [loading, setLoading] = useState(false)
+  const [loading, setLoading] = useState(false);
+  const [visible, setVisible] = useState(false);
+  const [errMsg, setErrMsg] = useState("");
+  const [alertTitle, setAlertTitile]= useState("")
 
   async function signInWithEmail() {
-    setLoading(true)
-    const { error } = await supabase.auth.signInWithPassword({
-      email: email,
-      password: password,
-    })
+    try{
+      setLoading(true)
+      const { error } = await supabase.auth.signInWithPassword({
+        email: email,
+        password: password,
+      })
 
-    if (error) Alert.alert(error.message)
-    setLoading(false)
+      if (error){
+        throw new Error(error.message)
+      }
+    }catch(err){
+      const msg = err?.message ? err.message : "message undefine";
+      setAlertTitile("Error")
+      setErrMsg(msg)
+      setVisible(true)
+      console.log('err', err)
+    }finally{
+      setLoading(false)
+    }
   }
 
   async function signUpWithEmail() {
-    setLoading(true)
-    const {
-      data: { session },
-      error,
-    } = await supabase.auth.signUp({
-      email: email,
-      password: password,
-    })
+    try{
+      setLoading(true)
+      const {
+        data: { session },
+        error,
+      } = await supabase.auth.signUp({
+        email: email,
+        password: password,
+      })
 
-    if (error) Alert.alert(error.message)
-    if (!session) Alert.alert('Please check your inbox for email verification!')
-    setLoading(false)
+      if (error){
+        throw new Error(error.message)
+      }
+
+      if (!session){
+        setAlertTitile("Info")
+        setErrMsg('Please check your inbox for email verification!')
+        setVisible(true)
+      }
+    }catch(err){
+      const msg = err?.message ? err.message : "message undefine";
+      setAlertTitile("Error")
+      setErrMsg(msg)
+      setVisible(true)
+      console.log('err', err)
+    }finally{
+      setLoading(false)
+    }
   }
 
   return (
     <View style={styles.container}>
       <View style={[styles.verticallySpaced, styles.mt20]}>
-        <Input
-          label="Email"
-          leftIcon={{ type: 'font-awesome', name: 'envelope' }}
+        <TextInput
           onChangeText={(text) => setEmail(text)}
           value={email}
           placeholder="email@address.com"
@@ -59,9 +83,7 @@ export default function Auth() {
         />
       </View>
       <View style={styles.verticallySpaced}>
-        <Input
-          label="Password"
-          leftIcon={{ type: 'font-awesome', name: 'lock' }}
+        <TextInput
           onChangeText={(text) => setPassword(text)}
           value={password}
           secureTextEntry={true}
@@ -75,6 +97,13 @@ export default function Auth() {
       <View style={styles.verticallySpaced}>
         <Button title="Sign up" disabled={loading} onPress={() => signUpWithEmail()} />
       </View>
+
+      <AlertDialog
+        visible={visible}
+        title={alertTitle}
+        message={errMsg}
+        onClose={() => setVisible(false)}
+      />
     </View>
   )
 }
--- lib/supabase.ts
+++ lib/supabase.ts
@@ -2,10 +2,7 @@ import AsyncStorage from '@react-native-async-storage/async-storage'
 import { createClient } from '@supabase/supabase-js'
 import { SUPABASE_URL, SUPABASE_ANON_KEY } from '@env'
 
-const supabaseUrl = SUPABASE_URL || ""
-const supabaseAnonKey = SUPABASE_ANON_KEY || ""
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
   auth: {
     storage: AsyncStorage,
     autoRefreshToken: true,
